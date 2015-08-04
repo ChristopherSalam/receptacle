408653247051e2058e8776bfdce35735a254a5fc
ChristopherSalam <christopher.salam@gmail.com>
2015-08-04 15:46:22 -0700

    Preparing for presentation and including multiple search options. Adding next steps and plan for encorporating APIs.

--- client/app/app.js
+++ client/app/app.js
@@ -2,8 +2,9 @@ var receptacle = angular.module('receptacle', [
   // 'receptacle.services',
   'receptacle.search',
   'ui.router'
-])
-  .config(function ($stateProvider, $urlRouterProvider) {
+]);
+
+receptacle.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('search', {
         url: '/',
@@ -11,9 +12,40 @@ var receptacle = angular.module('receptacle', [
         controller: 'SearchController'
       });
     $stateProvider
-      .state('contacts', {
-        template: '<h1>My Contacts</h1>'
+      .state('beta', {
+        url: '/beta',
+        template: '<h1>Next?</h1>' + '<h3>Finish preprocessor for all requests for trash in the local area</h3>',
+        controller: 'SearchController'
       });
     $urlRouterProvider
       .otherwise('/');
   });
+
+// receptacle.factory("sharedScope", function($rootScope) {
+//     var scope = $rootScope.$new(true);
+//     scope.data = [{type: 'cans', receptacle: 'blue bin'},
+//                   {type: 'can', receptacle: 'blue bin'},
+//                   {type: 'container', receptacle: 'blue bin'},
+//                   {type: 'soap bottles', receptacle: 'blue bin'},
+//                   {type: 'jars', receptacle: 'blue bin'},
+//                   {type: 'buckets', receptacle: 'blue bin'},
+//                   {type: 'aluminum', receptacle: 'blue bin'},
+//                   {type: 'glass', receptacle: 'blue bin'},
+//                   {type: 'bottle', receptacle: 'blue bin'},
+//                   {type: 'foil', receptacle: 'blue bin'},
+//                   {type: 'paper', receptacle: 'blue bin'},
+//                   {type: 'magazine', receptacle: 'blue bin'},
+//                   {type: 'food', receptacle: 'green bin'},
+//                   {type: 'newspaper', receptacle: 'blue bin'},
+//                   {type: 'fruits', receptacle: 'green bin'},
+//                   {type: 'vegetables', receptacle: 'green bin'},
+//                   {type: 'banana', receptacle: 'green bin'},
+//                   {type: 'carrots', receptacle: 'green bin'},
+//                   {type: 'bananas', receptacle: 'green bin'},
+//                   {type: 'carrot', receptacle: 'green bin'},
+//                   {type: 'styrafoam', receptacle: 'trash can'},
+//                   {type: 'plastic bags', receptacle: 'trash can'},
+//                   {type: 'plastic bags', receptacle: 'trash can'},
+//                   {type: 'shrinkwrap', receptacle: 'trash can'},];
+//     return scope;
+// });
--- client/app/search/search.js
+++ client/app/search/search.js
@@ -1,12 +1,22 @@
 angular.module('receptacle.search', [])
 
-  .controller('SearchController',
+   .controller('SearchController',
       ['$scope', '$location', '$anchorScroll',
         function ($scope, $location, $anchorScroll) {
-        $scope.length = this.length;
+        $scope.search = ""; //this is the key line.
+
         $scope.gotoBottom = function () {
 
-          $location.hash('bottom');
-          $anchorScroll();
-        };
-      }]);
+          $location.hash('bottom')
+          $anchorScroll()
+          };
+        }])
+
+
+// app.controller("first", function($scope, sharedScope) {
+//     $scope.data1 = sharedScope.data;
+// });
+
+// app.controller("second", function($scope, sharedScope) {
+//     $scope.data2 = sharedScope.data;
+// });

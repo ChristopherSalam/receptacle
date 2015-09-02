99e93bd44441266ee6a7e68472c680d8af4b9c90
Christopher Salam <christopher.salam@gmail.com>
2015-09-02 07:21:33 -0700

    Changed server to reflect new additions

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 var express = require('express'),
     hostname = process.env.HOSTNAME || 'localhost',
-    port = parseInt(process.env.PORT, 10) || 4567,
-    publicDir = process.argv[2] || __dirname + '/client',
+    port = process.env.PORT || 4567,
+    publicDir = __dirname + '/client',
     app = express();
-
-app.get("/", function (req, res) {
-  res.redirect("/");
-});
 
 app.use(express.static(publicDir));
 
-console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
-app.listen(port, hostname);
+app.get("/", function (req, res) {
+	res.render('index');
+});
+
+app.listen(port, function(){
+	console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
+});
 
 
 

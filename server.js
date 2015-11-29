8b89b05a59ce9aea49869def954f68ea8dcaae2e
ChristopherSalam <christopher.salam@gmail.com>
2015-11-28 22:47:37 -0800

    Receptacle now works with Firebase! Redis and Mongoose tomorrow.

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var express = require('express'),
     hostname = process.env.HOSTNAME || 'localhost',
     port = process.env.PORT || 4567,
     publicDir = __dirname + '/client',
+    nodeModules = __dirname +'/node_modules',
+    firebase = require('firebase'),
     app = express();
 
 app.use(express.static(publicDir));
+app.use(express.static(nodeModules));
 
 var viewRoute = require('./routes/view'),
     apiRoute = require('./routes/api');
